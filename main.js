806dad39b75f124ea3030ffb4986d4827b1cbd5a
mansa susa <mansasusa18@gmail.com>
2022-06-23 08:04:18 -0400

    fix linter errors

--- main.js
+++ main.js
@@ -1,7 +1,19 @@
-const hamburger = document.getElementById("hamburger");
-const dropdown = document.querySelector(".dropdown-container");
+const hamburger = document.getElementById('hamburger');
+const dropdown = document.querySelector('.dropdown-container');
+const times = document.getElementById('times');
+const menuItem = document.querySelectorAll('.dropdown-menu-item');
+let i;
 
-hamburger.addEventListener("click", () => {
-  dropdown.classList.toggle('active');
-})
+hamburger.addEventListener('click', () => {
+  dropdown.style.display = 'block';
+});
 
+times.addEventListener('click', () => {
+  dropdown.style.display = 'none';
+});
+
+for (i = 0; i < menuItem.length; ++i) {
+  menuItem[i].addEventListener('click', () => {
+    dropdown.style.display = 'none';
+  });
+}
