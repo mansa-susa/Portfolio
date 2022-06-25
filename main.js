be8b20a6c2119eaa0e1076dfe79a25944dbc6240
mansa susa <mansasusa18@gmail.com>
2022-06-25 10:27:37 -0400

    Add details popup window

--- main.js
+++ main.js
@@ -3,6 +3,13 @@ const dropdown = document.querySelector('.dropdown-container');
 const times = document.getElementById('times');
 const menuItem = document.querySelectorAll('.dropdown-menu-item');
 let i;
+const see_project = document.querySelectorAll('.project-button');
+const popup = document.querySelector('.popup-window');
+const popup_times = document.getElementById('popup-times');
+const wrapper = document.querySelector('.wrapper');
+let popup_heading = document.getElementById('popup-heding');
+popup_heading = 'Keeping track of hundreds  of components website';
+
 
 hamburger.addEventListener('click', () => {
   dropdown.style.display = 'block';
@@ -17,3 +24,15 @@ for (i = 0; i < menuItem.length; i += 1) {
     dropdown.style.display = 'none';
   });
 }
+
+for (i =0; i < see_project.length; i +=1) {
+  see_project[i].addEventListener('click', () => {
+    popup.style.display = 'block';
+    wrapper.style.opacity = 0.5;
+  });
+}
+
+popup_times.addEventListener('click', () => {
+  popup.style.display = 'none';
+  wrapper.style.opacity = 1;
+});
