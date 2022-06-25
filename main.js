7e8a1c37cc485109d291ca82a91bbcce6bd4f6f0
mansa susa <mansasusa18@gmail.com>
2022-06-25 10:48:50 -0400

    Fix linter errors

--- main.js
+++ main.js
@@ -3,13 +3,10 @@ const dropdown = document.querySelector('.dropdown-container');
 const times = document.getElementById('times');
 const menuItem = document.querySelectorAll('.dropdown-menu-item');
 let i;
-const see_project = document.querySelectorAll('.project-button');
+const seeProject = document.querySelectorAll('.project-button');
 const popup = document.querySelector('.popup-window');
-const popup_times = document.getElementById('popup-times');
+const popupTimes = document.getElementById('popup-times');
 const wrapper = document.querySelector('.wrapper');
-let popup_heading = document.getElementById('popup-heding');
-popup_heading = 'Keeping track of hundreds  of components website';
-
 
 hamburger.addEventListener('click', () => {
   dropdown.style.display = 'block';
@@ -25,14 +22,14 @@ for (i = 0; i < menuItem.length; i += 1) {
   });
 }
 
-for (i =0; i < see_project.length; i +=1) {
-  see_project[i].addEventListener('click', () => {
+for (i = 0; i < seeProject.length; i += 1) {
+  seeProject[i].addEventListener('click', () => {
     popup.style.display = 'block';
     wrapper.style.opacity = 0.5;
   });
 }
 
-popup_times.addEventListener('click', () => {
+popupTimes.addEventListener('click', () => {
   popup.style.display = 'none';
   wrapper.style.opacity = 1;
 });
